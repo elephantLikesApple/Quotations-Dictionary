07a6e77e3819aa50971f28c6b1087034d6081e26
elephantLikesApple <elephant890@naver.com>
2023-03-02 09:33:26 +0900

    step5. create '목록' command

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,10 +1,13 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
     public static Scanner sc = new Scanner(System.in);
-    public static int index = 0;
+    private static List<Quotation> quotationList = new ArrayList<>();
+    private static int index = 0;
     public static void main(String[] args) {
         System.out.println("== 명언 앱 ==");
         String command;
@@ -14,9 +17,11 @@ public class Main {
             if(command.equals("종료")) break;
             switch(command) {
                 case "등록":
-                    index++;
                     create();
                     System.out.println(index + "번 명언이 등록되었습니다.");
+                    break;
+                case "목록":
+                    listUp();
                     break;
                 default:
                     System.out.println("해결할 수 없는 명령입니다!");
@@ -25,9 +30,25 @@ public class Main {
     }
 
     public static void create() {
+        index++;
+        String context, author;
         System.out.printf("명언 : ");
-        sc.nextLine();
+        context = sc.nextLine();
         System.out.printf("작가 : ");
-        sc.nextLine();
+        author = sc.nextLine();
+
+        quotationList.add(new Quotation(context, author, index));
+    }
+
+    public static void listUp() {
+        System.out.println("번호 / 작가 / 명언");
+        System.out.println("----------------------");
+        for(int i = quotationList.size() - 1; i>=0 ; i--) {
+            System.out.printf("%d / %s / %s\n",
+                    quotationList.get(i).getNumber(),
+                    quotationList.get(i).getAuthor(),
+                    quotationList.get(i).getContext()
+            );
+        }
     }
 }
---
+++ src/main/java/org/example/Quotation.java
@@ -0,0 +1,37 @@
+package org.example;
+
+public class Quotation {
+    private int number;
+    private String context;
+    private String author;
+
+    public Quotation(String context, String author, int number) {
+        this.context = context;
+        this.author = author;
+        this.number = number;
+    }
+
+    public String getContext() {
+        return context;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setContext(String context) {
+        this.context = context;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+}
