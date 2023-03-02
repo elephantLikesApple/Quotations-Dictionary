80cb6629e67ba3fb2d415f4371628135a00ed3ae
elephantLikesApple <elephant890@naver.com>
2023-03-02 09:15:10 +0900

    step1. created '종료' command

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,16 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("== 명언 앱 ==");
+        String command;
+        while(true) {
+            System.out.printf("명령) ");
+            command = sc.nextLine();
+            if(command.equals("종료")) break;
+        }
     }
 }
