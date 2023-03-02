34c0ca6d51756983fe6ed85c61ef78f77c4c42c7
elephantLikesApple <elephant890@naver.com>
2023-03-02 09:18:15 +0900

    step2. created '등록' command

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,14 +3,29 @@ package org.example;
 import java.util.Scanner;
 
 public class Main {
+    public static Scanner sc = new Scanner(System.in);
+
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
         System.out.println("== 명언 앱 ==");
         String command;
         while(true) {
             System.out.printf("명령) ");
             command = sc.nextLine();
             if(command.equals("종료")) break;
+            switch(command) {
+                case "등록":
+                    create();
+                    break;
+                default:
+                    System.out.println("해결할 수 없는 명령입니다!");
+            }
         }
+    }
+
+    public static void create() {
+        System.out.printf("명언 : ");
+        sc.nextLine();
+        System.out.printf("작가 : ");
+        sc.nextLine();
     }
 }
